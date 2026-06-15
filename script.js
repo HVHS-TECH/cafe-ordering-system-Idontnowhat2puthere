289b8e1cc473f81d4d9977ca1ac9ae8ae86d1075
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-06-15 05:42:32 +0000

    Jun 15, 2026, 5:42 AM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello world!")
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    //Toggle moblie menu visibility
+    document.body.classList.toggle("show-moblie-menu");
+})
