da942619af012551922238803d5c1fc24e792068
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-06-15 05:49:36 +0000

    Jun 15, 2026, 5:49 AM

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 console.log("Hello world!")
-const menuOpenButton = document.querySelector("#menu-open-button");
+const navLinks = document.querySelector(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
     //Toggle moblie menu visibility
     document.body.classList.toggle("show-moblie-menu");
+});
+
+menuCloeButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click);
+});
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
 })
