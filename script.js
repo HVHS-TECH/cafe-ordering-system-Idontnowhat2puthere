a0fc7208329fbb1d7c14f33a8e1219f17a0ee05d
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-06-18 21:06:50 +0000

    Jun 18, 2026, 9:06 PM

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ const swiper = new Swiper('.slider-wrapper', {
     grabCursor: true,
     spaceBetween: 25,
 })
+
+
+function addItem() {
+
+    let item =
+        document.getElementById("shoppingInput").value;
+
+    if (item === "") {
+
+        OUTPUT.innerHTML +=
+            "<p>Please enter an item.</p>";
+
+        return;
+    }
+
+    shoppingList.push(item);
+
+    OUTPUT.innerHTML +=
+        "<p>You have added " +
+        item +
+        " to the list.</p>";
+
+    document.getElementById("shoppingInput").value = "";
+}
+
+function showList() {
+
+    OUTPUT.innerHTML +=
+        "<h3>These are the items on your shopping list:</h3>";
+
+    for (let i = 0; i < shoppingList.length; i++) {
+
+        OUTPUT.innerHTML +=
+            shoppingList[i] + "<br>";
+    }
+}
+
