c8462b1e3b384f4e80a8edc791f3eb0c714ec2b3
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-06-23 08:50:57 +0000

    Jun 23, 2026, 8:50 AM

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function addItem() {
         document.getElementById("itemInput").value;
 
     let money =
-        document.getElementById("money").value;
+        document.getElementById("moneyInput").value;
 
     shoppingList.push({
         name: name,
