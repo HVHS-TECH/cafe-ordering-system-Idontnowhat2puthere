94b684bd7a66e10cde9dd567ff14c1ace3ad87d8
Idontnowhat2puthere <24248jb@hvhs.school.nz>
2026-06-27 04:08:21 +0000

    Jun 27, 2026, 4:08 AM

--- script.js
+++ script.js
@@ -120,6 +120,39 @@ function addItem() {
     let money =
         Number(document.getElementById("moneyInput").value);
 
+    let price = 0;
+    
+    if (item ==="Small Size Package") {
+        price = 15;
+    }
+
+    if (item ==="Meduim Size Package") {
+        price = 35;
+    }
+
+    if (item ==="Large Size Package") {
+        price = 50;
+    }
+
+    if (item ==="Extra large Size Package") {
+        price = 100;
+    }
+
+    if (item ==="Retro Package") {
+        price = 75;
+    }
+
+    if (item ==="Global treasures Package") {
+        price = 120;
+    }
+
+    if (money < price) {
+        shouwOutput();
+        OUTPUT.innerHTML = 
+        "<p>You need at least $" + price + "for the " +item + "</p>";
+        return;
+    }
+
     shoppingList.push({
         name: name,
         item: item,
